8b2ccbf8602ebda207abbaf1391cfc0639492e88
aewshyae <1528058+aewshyae@users.noreply.github.com>
2020-12-30 02:03:22 +0000

    Update from https://github.com/aewshyae/aozorabunko-re-design/commit/67026649ed0d9187613505e31f42f357971d5c58)

---
+++ _nuxt/static/1609293594/book/1357/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Faozorabunko-re-design_pages\u002F_nuxt\u002Fstatic\u002F1609293594",layout:"default",error:null,state:{personDetailsArray:[],personDetails:{},worksArray:[],works:{}},serverRendered:true,routePath:"\u002Fbook\u002F1357",config:{}};
